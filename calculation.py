c856867a59e6b6b7a1fb1f96f948b1ff2bd8c694
Binoy Saha <binoy.saha01@northsouth.edu>
2022-06-28 00:00:39 +0600

    2

---
+++ calculation.py
@@ -0,0 +1,8 @@
+a= int(input('Enter 1st Number: '))
+b= int(input('Enter 2nd Number: '))
+
+print('Sum: ', a+b)
+print('Difference: ', a-b)
+print('product: ', a*b)
+print('ratio: ',a/b)
+print('reminder: ',a%b)
